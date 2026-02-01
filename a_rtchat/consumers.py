31caab8063c5458ecb0e32aaadf603e311c9e0ab
amin-bm <amin_bm2003@yahoo.com>
2026-02-01 13:36:02 +0330

    online count and color

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -19,6 +19,14 @@ class ChatroomConsumer(WebsocketConsumer):
         )
 
 
+        
+        # Add user and update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+            
+      
+        
         self.accept()
 
     def disconnect(self, close_code):
@@ -26,6 +34,11 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name,
             self.channel_name
         )
+        
+        # Remove user and update online users
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
     
 
     def receive(self, text_data):
@@ -57,4 +70,19 @@ class ChatroomConsumer(WebsocketConsumer):
         }
         html = render_to_string("a_rtchat/partials/chat_message_p.html", context=context)
         self.send(text_data=html)
+
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count() -1
+
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count,
+         }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+    
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+        html = render_to_string("a_rtchat/partials/online_count.html", {'online_count': online_count})
+        self.send(text_data=html)
+
        

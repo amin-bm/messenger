0d26b59492a1a0b690f837e3f094a293e9d187f0
amin-bm <161292458+amin-bm@users.noreply.github.com>
2026-02-06 17:09:32 +0330

    voice message ui

--- a_rtchat/forms.py
+++ a_rtchat/forms.py
@@ -9,7 +9,7 @@ class ChatmessageCreateForm(ModelForm):
         model = GroupMessage
         fields = ('body',)
         widgets = {
-            'body': forms.TextInput(attrs={'placeholder': 'Add message...', 'class': 'p-4 text-black', 'maxlength': '300', 'autofocus': True, 'autocomplete': 'off'}),
+            'body': forms.TextInput(attrs={'placeholder': 'Write a message...', 'class': 'w-full bg-transparent px-2 py-2 text-sm text-gray-900 placeholder-gray-400 outline-none', 'maxlength': '300', 'autofocus': True, 'autocomplete': 'off'}),
         }
 
 

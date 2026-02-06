d2c09d6b9696171c689eb1092685d072a095cd88
amin-bm <161292458+amin-bm@users.noreply.github.com>
2026-02-06 19:32:28 +0330

    RTL text input and show messages

--- a_rtchat/forms.py
+++ a_rtchat/forms.py
@@ -9,7 +9,7 @@ class ChatmessageCreateForm(ModelForm):
         model = GroupMessage
         fields = ('body',)
         widgets = {
-            'body': forms.Textarea(attrs={'placeholder': 'Write a message...', 'class': 'w-full !bg-transparent !px-2 !py-2 !rounded-none text-sm text-gray-900 placeholder-gray-400 outline-none resize-none leading-6 max-h-40 overflow-y-auto', 'maxlength': '300', 'rows': 1, 'autofocus': True, 'autocomplete': 'off'}),
+            'body': forms.Textarea(attrs={'placeholder': 'Write a message...', 'class': 'w-full !bg-transparent !px-2 !py-2 !rounded-none text-sm text-gray-900 placeholder-gray-400 outline-none resize-none leading-6 max-h-40 overflow-y-auto', 'style': 'unicode-bidi: plaintext; text-align: start;', 'dir': 'auto', 'maxlength': '300', 'rows': 1, 'autofocus': True, 'autocomplete': 'off'}),
         }
 
 

7db8f64bd944c2a47d47eb110843a285ac844e2e
amin-bm <161292458+amin-bm@users.noreply.github.com>
2026-02-06 19:25:19 +0330

    long text input

--- a_rtchat/forms.py
+++ a_rtchat/forms.py
@@ -9,7 +9,7 @@ class ChatmessageCreateForm(ModelForm):
         model = GroupMessage
         fields = ('body',)
         widgets = {
-            'body': forms.TextInput(attrs={'placeholder': 'Write a message...', 'class': 'w-full bg-transparent px-2 py-2 text-sm text-gray-900 placeholder-gray-400 outline-none', 'maxlength': '300', 'autofocus': True, 'autocomplete': 'off'}),
+            'body': forms.Textarea(attrs={'placeholder': 'Write a message...', 'class': 'w-full !bg-transparent !px-2 !py-2 !rounded-none text-sm text-gray-900 placeholder-gray-400 outline-none resize-none leading-6 max-h-40 overflow-y-auto', 'maxlength': '300', 'rows': 1, 'autofocus': True, 'autocomplete': 'off'}),
         }
 
 

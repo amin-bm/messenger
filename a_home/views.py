03fe7f807eade23a0ff3ceb04b9a44cf9c5e05b8
amin-bm <161292458+amin-bm@users.noreply.github.com>
2026-02-08 18:52:39 +0330

    correction PWA error

--- a_home/views.py
+++ a_home/views.py
@@ -1,8 +1,9 @@
 import json
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.staticfiles import finders
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.http import HttpResponse, JsonResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_GET, require_POST
 
@@ -16,16 +17,39 @@ def home_view(request):
 
 @require_GET
 def pwa_manifest(request):
-    with staticfiles_storage.open("manifest.webmanifest", "rb") as f:
-        content = f.read()
-    return HttpResponse(content, content_type="application/manifest+json")
+    name = "manifest.webmanifest"
+    try:
+        with staticfiles_storage.open(name, "rb") as f:
+            content = f.read()
+    except FileNotFoundError:
+        path = finders.find(name)
+        if not path:
+            raise Http404()
+        with open(path, "rb") as f:
+            content = f.read()
+
+    response = HttpResponse(content, content_type="application/manifest+json")
+    response["Cache-Control"] = "no-cache"
+    return response
 
 
 @require_GET
 def pwa_service_worker(request):
-    with staticfiles_storage.open("sw.js", "rb") as f:
-        content = f.read()
-    return HttpResponse(content, content_type="application/javascript")
+    name = "sw.js"
+    try:
+        with staticfiles_storage.open(name, "rb") as f:
+            content = f.read()
+    except FileNotFoundError:
+        path = finders.find(name)
+        if not path:
+            raise Http404()
+        with open(path, "rb") as f:
+            content = f.read()
+
+    response = HttpResponse(content, content_type="application/javascript")
+    response["Cache-Control"] = "no-cache"
+    response["Service-Worker-Allowed"] = "/"
+    return response
 
 
 @require_GET

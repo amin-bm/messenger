14e271ce8593e88f54cea75df59612dfc84e4081
amin-bm <161292458+amin-bm@users.noreply.github.com>
2026-02-11 20:31:01 +0330

    one tap mobile chats open

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "pesk-messenger-v5";
+const CACHE_NAME = "pesk-messenger-v8";
 
 const APP_SHELL = [
   "/",
   "/static/css/tailwind.css?v=3",
   "/static/css/fonts.css",
-  "/static/css/style.css?v=2",
+  "/static/css/style.css?v=5",
   "/static/vendor/htmx.min.js",
   "/static/vendor/ws.min.js",
   "/static/vendor/alpine.min.js",
